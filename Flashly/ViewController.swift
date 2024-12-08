db3120e8e1680ba087535b84c8cae3233178317c
ronaldo-avalos <ronaldoadan1@gmail.com>
2024-12-08 16:00:06 -0600

    Initial Commit

---
+++ Flashly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flashly
+//
+//  Created by Ronaldo Avalos on 08/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
